3cfba012124043f7fdd4362523c2cef28264603e
Asteria Moon <asteriasmoons@outlook.com>
2025-05-21 12:47:14 -0500

    changed the require

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,7 +1,7 @@
 // interactionCreate.js
 const { EmbedBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 const EmbedModel = require('../models/Embed');
-const { buildEmbed } = require('../utils/embedEditorUi.js');
+const { buildEmbed } = require('../utils/embedEditorUI');
 
 module.exports = {
   name: 'interactionCreate',
