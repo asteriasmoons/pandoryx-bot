83ad47eb45d60dab8b3e3a0bd92725e8f26a8e8a
Asteria Moon <asteriasmoons@outlook.com>
2025-07-06 17:40:43 -0500

    Deleted outside premium bullshit

--- models/Premium.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const premiumSchema = new mongoose.Schema({
-  discordId: { type: String, required: true, unique: true },
-  tier: { type: String, default: "Supporter" },
-  activatedAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model("Premium", premiumSchema, "premiums");
